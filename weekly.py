0d6e204c0db6a69c0a86e5d6f8c908f294028606
Kurt Mosiejczuk <kurt@cranky.work>
2021-03-05 23:14:23 -0500

    Update to use latest beta, 21w08b

--- weekly.py
+++ weekly.py
@@ -4,7 +4,7 @@ import minecraft_launcher_lib as mll
 import subprocess
 
 # Minecraft version
-mc_version = "21w07a"
+mc_version = "21w08b"
 
 # Asset index is same but without final revision
 asset_index = "1.17"
