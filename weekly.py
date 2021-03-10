36f755dbfc13d275f5a06daec0c1e3108300ad82
Kurt Mosiejczuk <kurt@cranky.work>
2021-03-10 11:35:56 -0500

    Updated for new 21w10a "Lush Caves" snapshot

--- weekly.py
+++ weekly.py
@@ -4,7 +4,7 @@ import minecraft_launcher_lib as mll
 import subprocess
 
 # Minecraft version
-mc_version = "21w08b"
+mc_version = "21w10a"
 
 # Asset index is same but without final revision
 asset_index = "1.17"
