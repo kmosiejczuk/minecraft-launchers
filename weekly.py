b810db5fe8906803fa5e4c89d5e946064dd240cb
Kurt Mosiejczuk <kurt@cranky.work>
2021-05-07 13:32:58 -0400

    Update to new 21w18a

--- weekly.py
+++ weekly.py
@@ -4,7 +4,7 @@ import minecraft_launcher_lib as mll
 import subprocess
 
 # Minecraft version
-mc_version = "21w16a"
+mc_version = "21w18a"
 
 # Asset index is same but without final revision
 asset_index = "1.17"
