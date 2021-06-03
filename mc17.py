641215007ebf28be57fec82ea7981ffdcf3823eb
Kurt Mosiejczuk <kurt@cranky.work>
2021-06-02 21:17:40 -0400

    The march to release continues. Update 1.17 to pre-release 4

--- mc17.py
+++ mc17.py
@@ -4,7 +4,7 @@ import minecraft_launcher_lib as mll
 import subprocess
 
 # Minecraft version
-mc_version = "1.17-pre2"
+mc_version = "1.17-pre4"
 
 # Asset index is same but without final revision
 asset_index = "1.17"
