7ca9c5010c7f8e25766968112e7fee0259adf11a
Kurt Mosiejczuk <kurt@cranky.work>
2021-10-17 22:13:46 -0400

    Update weekly script to use 1.18 snapshots (21w41a)

--- weekly.py
+++ weekly.py
@@ -4,10 +4,10 @@ import minecraft_launcher_lib as mll
 import subprocess
 
 # Minecraft version
-mc_version = "1.17.1-rc1"
+mc_version = "21w41a"
 
 # Asset index is same but without final revision
-asset_index = "1.17"
+asset_index = "1.18"
 
 # Your email, username and password below
 login =    "yourEmailUsername"
@@ -65,25 +65,25 @@ real_command = [
   '-cp',
   libdir + 'com/mojang/blocklist/1.0.5/blocklist-1.0.5.jar:'
   + libdir + 'com/mojang/patchy/2.1.6/patchy-2.1.6.jar:'
-  + libdir + 'com/github/oshi/oshi-core/5.7.4/oshi-core-5.7.4.jar:'
+  + libdir + 'com/github/oshi/oshi-core/5.7.5/oshi-core-5.7.5.jar:'
   + libdir + 'net/java/dev/jna/jna/5.8.0/jna-5.8.0.jar:'
   + libdir + 'net/java/dev/jna/jna-platform/5.8.0/jna-platform-5.8.0.jar:'
   + libdir + 'org/slf4j/slf4j-api/1.8.0-beta4/slf4j-api-1.8.0-beta4.jar:'
   + libdir + 'org/apache/logging/log4j/log4j-slf4j18-impl/2.14.1/log4j-slf4j18-impl-2.14.1.jar:'
   + libdir + 'com/ibm/icu/icu4j/66.1/icu4j-66.1.jar:'
-  + libdir + 'com/mojang/javabridge/1.1.23/javabridge-1.1.23.jar:'
+  + libdir + 'com/mojang/javabridge/1.2.24/javabridge-1.2.24.jar:'
   + libdir + 'net/sf/jopt-simple/jopt-simple/5.0.3/jopt-simple-5.0.3.jar:'
   + libdir + 'io/netty/netty-all/4.1.25.Final/netty-all-4.1.25.Final.jar:'
   + libdir + 'com/google/guava/guava/21.0/guava-21.0.jar:'
   + libdir + 'org/apache/commons/commons-lang3/3.5/commons-lang3-3.5.jar:'
   + libdir + 'commons-io/commons-io/2.5/commons-io-2.5.jar:'
-  + libdir + 'commons-codec/commons-codec/1.10/commons-codec-1.10.jar:'
+  + libdir + 'commons-codec/commons-codec/1.15/commons-codec-1.15.jar:'
   + libdir + 'net/java/jinput/jinput/2.0.5/jinput-2.0.5.jar:'
   + libdir + 'net/java/jutils/jutils/1.0.0/jutils-1.0.0.jar:'
   + libdir + 'com/mojang/brigadier/1.0.18/brigadier-1.0.18.jar:'
   + libdir + 'com/mojang/datafixerupper/4.0.26/datafixerupper-4.0.26.jar:'
   + libdir + 'com/google/code/gson/gson/2.8.0/gson-2.8.0.jar:'
-  + libdir + 'com/mojang/authlib/2.2.30/authlib-2.2.30.jar:'
+  + libdir + 'com/mojang/authlib/3.1.36/authlib-3.1.36.jar:'
   + libdir + 'org/apache/commons/commons-compress/1.8.1/commons-compress-1.8.1.jar:'
   + libdir + 'org/apache/httpcomponents/httpclient/4.3.3/httpclient-4.3.3.jar:'
   + libdir + 'commons-logging/commons-logging/1.1.3/commons-logging-1.1.3.jar:'
