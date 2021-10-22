3c341ad0be79a879024af44aa14af354402a2eed
Kurt Mosiejczuk <kurt@cranky.work>
2021-10-22 15:46:47 -0400

    Update to the latest snapshot - 21w42a
    Mojang seems to have gone on an updating spree. Many of the libraries used
    for this version are much newer than before. The jinput and jutils libraries
    also seem to have been dropped.

--- weekly.py
+++ weekly.py
@@ -4,7 +4,7 @@ import minecraft_launcher_lib as mll
 import subprocess
 
 # Minecraft version
-mc_version = "21w41a"
+mc_version = "21w42a"
 
 # Asset index is same but without final revision
 asset_index = "1.18"
@@ -63,32 +63,31 @@ real_command = [
   '-Dminecraft.launcher.brand=minecraft-launcher-lib',
   '-Dminecraft.launcher.version=2.1',
   '-cp',
-  libdir + 'com/mojang/blocklist/1.0.5/blocklist-1.0.5.jar:'
+  libdir + 'com/mojang/blocklist/1.0.6/blocklist-1.0.6.jar:'
   + libdir + 'com/mojang/patchy/2.1.6/patchy-2.1.6.jar:'
-  + libdir + 'com/github/oshi/oshi-core/5.7.5/oshi-core-5.7.5.jar:'
-  + libdir + 'net/java/dev/jna/jna/5.8.0/jna-5.8.0.jar:'
-  + libdir + 'net/java/dev/jna/jna-platform/5.8.0/jna-platform-5.8.0.jar:'
+  + libdir + 'com/github/oshi/oshi-core/5.8.2/oshi-core-5.8.2.jar:'
+  + libdir + 'net/java/dev/jna/jna/5.9.0/jna-5.9.0.jar:'
+  + libdir + 'net/java/dev/jna/jna-platform/5.9.0/jna-platform-5.9.0.jar:'
   + libdir + 'org/slf4j/slf4j-api/1.8.0-beta4/slf4j-api-1.8.0-beta4.jar:'
   + libdir + 'org/apache/logging/log4j/log4j-slf4j18-impl/2.14.1/log4j-slf4j18-impl-2.14.1.jar:'
-  + libdir + 'com/ibm/icu/icu4j/66.1/icu4j-66.1.jar:'
+  + libdir + 'com/ibm/icu/icu4j/69.1/icu4j-69.1.jar:'
   + libdir + 'com/mojang/javabridge/1.2.24/javabridge-1.2.24.jar:'
-  + libdir + 'net/sf/jopt-simple/jopt-simple/5.0.3/jopt-simple-5.0.3.jar:'
-  + libdir + 'io/netty/netty-all/4.1.25.Final/netty-all-4.1.25.Final.jar:'
-  + libdir + 'com/google/guava/guava/21.0/guava-21.0.jar:'
-  + libdir + 'org/apache/commons/commons-lang3/3.5/commons-lang3-3.5.jar:'
-  + libdir + 'commons-io/commons-io/2.5/commons-io-2.5.jar:'
+  + libdir + 'net/sf/jopt-simple/jopt-simple/5.0.4/jopt-simple-5.0.4.jar:'
+  + libdir + 'io/netty/netty-all/4.1.68.Final/netty-all-4.1.68.Final.jar:'
+  + libdir + 'com/google/guava/failureaccess/1.0.1/failureaccess-1.0.1.jar:'
+  + libdir + 'com/google/guava/guava/31.0.1-jre/guava-31.0.1-jre.jar:'
+  + libdir + 'org/apache/commons/commons-lang3/3.12.0/commons-lang3-3.12.0.jar:'
+  + libdir + 'commons-io/commons-io/2.11.0/commons-io-2.11.0.jar:'
   + libdir + 'commons-codec/commons-codec/1.15/commons-codec-1.15.jar:'
-  + libdir + 'net/java/jinput/jinput/2.0.5/jinput-2.0.5.jar:'
-  + libdir + 'net/java/jutils/jutils/1.0.0/jutils-1.0.0.jar:'
   + libdir + 'com/mojang/brigadier/1.0.18/brigadier-1.0.18.jar:'
   + libdir + 'com/mojang/datafixerupper/4.0.26/datafixerupper-4.0.26.jar:'
-  + libdir + 'com/google/code/gson/gson/2.8.0/gson-2.8.0.jar:'
+  + libdir + 'com/google/code/gson/gson/2.8.8/gson-2.8.8.jar:'
   + libdir + 'com/mojang/authlib/3.1.36/authlib-3.1.36.jar:'
-  + libdir + 'org/apache/commons/commons-compress/1.8.1/commons-compress-1.8.1.jar:'
-  + libdir + 'org/apache/httpcomponents/httpclient/4.3.3/httpclient-4.3.3.jar:'
-  + libdir + 'commons-logging/commons-logging/1.1.3/commons-logging-1.1.3.jar:'
-  + libdir + 'org/apache/httpcomponents/httpcore/4.3.2/httpcore-4.3.2.jar:'
-  + libdir + 'it/unimi/dsi/fastutil/8.2.1/fastutil-8.2.1.jar:'
+  + libdir + 'org/apache/commons/commons-compress/1.21/commons-compress-1.21.jar:'
+  + libdir + 'org/apache/httpcomponents/httpclient/4.5.13/httpclient-4.5.13.jar:'
+  + libdir + 'commons-logging/commons-logging/1.2/commons-logging-1.2.jar:'
+  + libdir + 'org/apache/httpcomponents/httpcore/4.4.14/httpcore-4.4.14.jar:'
+  + libdir + 'it/unimi/dsi/fastutil/8.5.6/fastutil-8.5.6.jar:'
   + libdir + 'org/apache/logging/log4j/log4j-api/2.14.1/log4j-api-2.14.1.jar:'
   + libdir + 'org/apache/logging/log4j/log4j-core/2.14.1/log4j-core-2.14.1.jar:'
   + lwjgl3_libs
