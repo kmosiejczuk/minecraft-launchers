8af8fc3aa5f58d9c8cbb881cbf5c6842f81a2998
Kurt Mosiejczuk <kurt@cranky.work>
2021-10-27 18:21:02 -0400

    Bump version number for new 21w43a snapshot (No library changes this time)

--- weekly.py
+++ weekly.py
@@ -4,7 +4,7 @@ import minecraft_launcher_lib as mll
 import subprocess
 
 # Minecraft version
-mc_version = "21w42a"
+mc_version = "21w43a"
 
 # Asset index is same but without final revision
 asset_index = "1.18"
