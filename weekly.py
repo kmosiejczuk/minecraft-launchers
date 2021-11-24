1112fe0ddd9b0668a13e59ab305f2b9eea0a48a8
Kurt Mosiejczuk <kurt@cranky.work>
2021-11-24 12:00:21 -0500

    Another day, another pre-release (8)

--- weekly.py
+++ weekly.py
@@ -4,7 +4,7 @@ import minecraft_launcher_lib as mll
 import subprocess
 
 # Minecraft version
-mc_version = "1.18-pre7"
+mc_version = "1.18-pre8"
 
 # Asset index is same but without final revision
 asset_index = "1.18"
