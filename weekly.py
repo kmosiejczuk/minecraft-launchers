78589d2718ade6670ea0c491b36625f706c0ba3d
Kurt Mosiejczuk <kurt@cranky.work>
2021-12-08 10:20:24 -0500

    Update for 1.18.1 release candidate 2

--- weekly.py
+++ weekly.py
@@ -4,7 +4,7 @@ import minecraft_launcher_lib as mll
 import subprocess
 
 # Minecraft version
-mc_version = "1.18.1-pre1"
+mc_version = "1.18.1-rc2"
 
 # Asset index is same but without final revision
 asset_index = "1.18"
