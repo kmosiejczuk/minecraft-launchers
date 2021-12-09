a1bd41c74256b19c0e6be7975ac3bdacd691f2c8
Kurt Mosiejczuk <kurt@cranky.work>
2021-12-09 11:55:37 -0500

    Make mc15 put the trailing slash after libraries into libdir like every other launcher script

--- mc15.py
+++ mc15.py
@@ -16,7 +16,7 @@ password = "seekritPasswordHere"
 # Get Minecraft directory
 mc_directory = mll.utils.get_minecraft_directory()
 
-libdir = mc_directory + '/libraries'
+libdir = mc_directory + '/libraries/'
 
 lwjgl3_libs = '/usr/local/share/lwjgl3/lwjgl.jar:' \
   + '/usr/local/share/lwjgl3/lwjgl-openal.jar:' \
@@ -63,33 +63,33 @@ real_command = [
   '-Dminecraft.launcher.brand=minecraft-launcher-lib',
   '-Dminecraft.launcher.version=2.1',
   '-cp',
-  libdir + '/com/mojang/patchy/1.1/patchy-1.1.jar:'
-  + libdir + '/oshi-project/oshi-core/1.1/oshi-core-1.1.jar:'
-  + libdir + '/net/java/dev/jna/jna/4.4.0/jna-4.4.0.jar:'
-  + libdir + '/net/java/dev/jna/platform/3.4.0/platform-3.4.0.jar:'
-  + libdir + '/com/ibm/icu/icu4j-core-mojang/51.2/icu4j-core-mojang-51.2.jar:'
-  + libdir + '/com/mojang/javabridge/1.0.22/javabridge-1.0.22.jar:'
-  + libdir + '/net/sf/jopt-simple/jopt-simple/5.0.3/jopt-simple-5.0.3.jar:'
-  + libdir + '/io/netty/netty-all/4.1.25.Final/netty-all-4.1.25.Final.jar:'
-  + libdir + '/com/google/guava/guava/21.0/guava-21.0.jar:'
-  + libdir + '/org/apache/commons/commons-lang3/3.5/commons-lang3-3.5.jar:'
-  + libdir + '/commons-io/commons-io/2.5/commons-io-2.5.jar:'
-  + libdir + '/commons-codec/commons-codec/1.10/commons-codec-1.10.jar:'
-  + libdir + '/net/java/jinput/jinput/2.0.5/jinput-2.0.5.jar:'
-  + libdir + '/net/java/jutils/jutils/1.0.0/jutils-1.0.0.jar:'
-  + libdir + '/com/mojang/brigadier/1.0.17/brigadier-1.0.17.jar:'
-  + libdir + '/com/mojang/datafixerupper/2.0.24/datafixerupper-2.0.24.jar:'
-  + libdir + '/com/google/code/gson/gson/2.8.0/gson-2.8.0.jar:'
-  + libdir + '/com/mojang/authlib/1.5.25/authlib-1.5.25.jar:'
-  + libdir + '/org/apache/commons/commons-compress/1.8.1/commons-compress-1.8.1.jar:'
-  + libdir + '/org/apache/httpcomponents/httpclient/4.3.3/httpclient-4.3.3.jar:'
-  + libdir + '/commons-logging/commons-logging/1.1.3/commons-logging-1.1.3.jar:'
-  + libdir + '/org/apache/httpcomponents/httpcore/4.3.2/httpcore-4.3.2.jar:'
-  + libdir + '/it/unimi/dsi/fastutil/8.2.1/fastutil-8.2.1.jar:'
-  + libdir + '/org/apache/logging/log4j/log4j-api/2.8.1/log4j-api-2.8.1.jar:'
-  + libdir + '/org/apache/logging/log4j/log4j-core/2.8.1/log4j-core-2.8.1.jar:'
+  libdir + 'com/mojang/patchy/1.1/patchy-1.1.jar:'
+  + libdir + 'oshi-project/oshi-core/1.1/oshi-core-1.1.jar:'
+  + libdir + 'net/java/dev/jna/jna/4.4.0/jna-4.4.0.jar:'
+  + libdir + 'net/java/dev/jna/platform/3.4.0/platform-3.4.0.jar:'
+  + libdir + 'com/ibm/icu/icu4j-core-mojang/51.2/icu4j-core-mojang-51.2.jar:'
+  + libdir + 'com/mojang/javabridge/1.0.22/javabridge-1.0.22.jar:'
+  + libdir + 'net/sf/jopt-simple/jopt-simple/5.0.3/jopt-simple-5.0.3.jar:'
+  + libdir + 'io/netty/netty-all/4.1.25.Final/netty-all-4.1.25.Final.jar:'
+  + libdir + 'com/google/guava/guava/21.0/guava-21.0.jar:'
+  + libdir + 'org/apache/commons/commons-lang3/3.5/commons-lang3-3.5.jar:'
+  + libdir + 'commons-io/commons-io/2.5/commons-io-2.5.jar:'
+  + libdir + 'commons-codec/commons-codec/1.10/commons-codec-1.10.jar:'
+  + libdir + 'net/java/jinput/jinput/2.0.5/jinput-2.0.5.jar:'
+  + libdir + 'net/java/jutils/jutils/1.0.0/jutils-1.0.0.jar:'
+  + libdir + 'com/mojang/brigadier/1.0.17/brigadier-1.0.17.jar:'
+  + libdir + 'com/mojang/datafixerupper/2.0.24/datafixerupper-2.0.24.jar:'
+  + libdir + 'com/google/code/gson/gson/2.8.0/gson-2.8.0.jar:'
+  + libdir + 'com/mojang/authlib/1.5.25/authlib-1.5.25.jar:'
+  + libdir + 'org/apache/commons/commons-compress/1.8.1/commons-compress-1.8.1.jar:'
+  + libdir + 'org/apache/httpcomponents/httpclient/4.3.3/httpclient-4.3.3.jar:'
+  + libdir + 'commons-logging/commons-logging/1.1.3/commons-logging-1.1.3.jar:'
+  + libdir + 'org/apache/httpcomponents/httpcore/4.3.2/httpcore-4.3.2.jar:'
+  + libdir + 'it/unimi/dsi/fastutil/8.2.1/fastutil-8.2.1.jar:'
+  + libdir + 'org/apache/logging/log4j/log4j-api/2.8.1/log4j-api-2.8.1.jar:'
+  + libdir + 'org/apache/logging/log4j/log4j-core/2.8.1/log4j-core-2.8.1.jar:'
   + lwjgl3_libs
-  + libdir + '/com/mojang/text2speech/1.11.3/text2speech-1.11.3.jar:'
+  + libdir + 'com/mojang/text2speech/1.11.3/text2speech-1.11.3.jar:'
   + mc_directory + '/versions/' + mc_version + '/' + mc_version + '.jar',
   'net.minecraft.client.main.Main',
   '--username', username,
